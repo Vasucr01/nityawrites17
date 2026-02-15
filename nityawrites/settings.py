4cafda31d6e20ac35877561adb18cb470a6d4597
kevalchauhan <vasuchauhan202020@gmail.com>
2026-02-15 12:10:56 +0530

    Configure project for external PostgreSQL database

--- nityawrites/settings.py
+++ nityawrites/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +79,21 @@ WSGI_APPLICATION = 'nityawrites.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES["default"] = dj_database_url.parse(database_url)
+    DATABASES["default"]["OPTIONS"] = {
+        "sslmode": "require",  # Enabled for Vercel/Neon Postgres
+    }
 
 
 # Password validation
